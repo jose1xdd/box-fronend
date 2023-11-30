728b2aa0fa27156951730ca08fa90658feeab2f7
juliana-navarro <lizethjuliananava@ufps.edu.co>
2023-11-29 19:47:52 -0500

    Jerarquía de carpetas actualizada

--- app/administrador/layout.tsx
+++ app/administrador/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import NavbarAdministrador from '@/components/navbarAdministrador';
+import NavbarAdministrador from '@/components/navbar/navbarAdministrador';
 
 export const metadata: Metadata = {
 	title: 'Administrador'
