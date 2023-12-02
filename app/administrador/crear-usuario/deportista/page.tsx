c2ab9e25553968423b40ca1c10eee12b7a2faad2
juliana-navarro <lizethjuliananava@ufps.edu.co>
2023-12-02 15:21:25 -0500

    Ranking mal

--- app/administrador/crear-usuario/deportista/page.tsx
+++ app/administrador/crear-usuario/deportista/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { ChangeEvent, useState } from 'react';
+import OpcionesClubes from '@/components/OpcionesClubes';
+import fechaCompleta from '@/app/types/funcionesDate';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import OpcionesCategorias from '@/components/OpcionesCategorias';
+import router from 'next/router';
 
 interface FormData {
   nombre: string;
@@ -11,6 +16,8 @@ interface FormData {
   correo: string;
   club: string;
   categoria: string;
+  peso: number;
+  fecha: Date;
 }
 
 export default function CrearDeportista() {
@@ -24,23 +31,104 @@ export default function CrearDeportista() {
 		correo: '',
 		club: '',
 		categoria: '',
+		peso: 0,
+		fecha: new Date(),
 	});
 
-	const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-		setDatosNuevoDeportista((prevData) => ({
-			...prevData,
+	const handleChange = (field: keyof FormData, value: string) => {
+		setDatosNuevoDeportista((prevFormData) => ({
+			...prevFormData,
+			[field]: value
 		}));
 	};
 
-	/*const handleGuardarCambios = () => {
-		axios.post('/', datosNuevoDeportista)
-			.then((response) => {
-				console.log('Datos guardados exitosamente', response.data);
-			})
-			.catch((error) => {
-				console.error('Error al guardar los datos', error);
+	const handleChangeFecha = (field: keyof FormData, value: string) => {
+		var fecha = new Date(value);
+		setDatosNuevoDeportista((prevFormData) => ({
+			...prevFormData,
+			[field]: fecha
+		}));
+	};
+
+	async function handleGuardarCambios(): Promise<void> {
+		const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+		try{
+			const datos = localStorage.getItem('userData');
+			var arreglo;
+
+			if (datos != null) {
+				arreglo = JSON.parse(datos);
+			}
+
+			const cabeza = {
+				sessiontoken: arreglo.token,
+			};
+
+			const body = {
+				name: datosNuevoDeportista.nombre,
+				lastName: datosNuevoDeportista.apellido,
+				birthDate: fechaCompleta(datosNuevoDeportista.fecha),
+				weight: Number(datosNuevoDeportista.peso),
+				cedula: datosNuevoDeportista.documento,
+				email: datosNuevoDeportista.correo,
+				phone: datosNuevoDeportista.telefono,
+				club: datosNuevoDeportista.club,
+				weightCategory: datosNuevoDeportista.categoria,
+				address: datosNuevoDeportista.direccion
+			};
+
+			const response = await axios.post(`${apiEndpoint}/users/Deportista`, body, {
+				headers: cabeza,
 			});
-	};*/
+			window.location.href = `/administrador/info-usuario/deportista?id=${response.data.user._id}`;
+		} catch (error) {
+			console.log(error);
+		}
+
+	};
+
+	/////////MÉTODO PARA TRAER LOS CLUBES
+	var cargado = false;
+	const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+	useEffect(() => {
+		const datos = localStorage.getItem('userData');
+		var arreglo;
+
+		if (datos != null) {
+			arreglo = JSON.parse(datos);
+		}
+
+		//carga(arreglo);
+		cargado = true;
+	}, [!cargado]);
+
+	async function carga(datos: { token: any; userId: any }): Promise<void> {
+		try {
+			const headers = {
+				sessiontoken: datos.token
+			};
+			const parametros = {
+				userId: datos.userId
+			};
+
+			const response = await axios.get(`${apiEndpoint}/users`, {
+				params: parametros,
+				headers: headers
+			});
+
+			// Actualizar el estado con los datos recibidos
+			/* setDatosPerfil({
+				nombre: response.data.user.name,
+				apellido: response.data.user.lastName,
+				cedula: response.data.user.cedula,
+				direccion: response.data.user.address,
+				telefono: response.data.user.phone,
+				correo: response.data.user.email,
+			}); */
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
 	return (
 		<>
@@ -71,7 +159,7 @@ export default function CrearDeportista() {
 										type="text"
 										name="nombre"
 										value={datosNuevoDeportista.nombre}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('nombre', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 										placeholder='Ingrese el nombre'
 									/>
@@ -88,7 +176,7 @@ export default function CrearDeportista() {
 										type="text"
 										name="apellido"
 										value={datosNuevoDeportista.apellido}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('apellido', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 										placeholder='Ingrese el apellido'
 									/>
@@ -105,7 +193,7 @@ export default function CrearDeportista() {
 										type="text"
 										name="documento"
 										value={datosNuevoDeportista.documento}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('documento', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 										placeholder='Ingrese el número de documento'
 									/>
@@ -122,9 +210,26 @@ export default function CrearDeportista() {
 										type="text"
 										name="direccion"
 										value={datosNuevoDeportista.direccion}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('direccion', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 										placeholder='Ingrese la dirección'
+									/>
+								</div>
+							</div>
+							<div className="flex">
+								<div className="w-1/3 mx-2">
+									<div className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 flex items-center justify-center text-black' id='texto-general'>
+                                        Peso
+									</div>
+								</div>
+								<div className="w-2/3 mx-2" id='texto-general'>
+									<input
+										type="number"
+										name="peso"
+										value={datosNuevoDeportista.peso}
+										onChange={(e) => handleChange('peso', e.target.value)}
+										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
+										placeholder='Ingresa el peso'
 									/>
 								</div>
 							</div>
@@ -141,7 +246,7 @@ export default function CrearDeportista() {
 										type="text"
 										name="telefono"
 										value={datosNuevoDeportista.telefono}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('telefono', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 										placeholder='Ingrese el teléfono'
 									/>
@@ -158,7 +263,7 @@ export default function CrearDeportista() {
 										type="text"
 										name="correo"
 										value={datosNuevoDeportista.correo}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('correo', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 										placeholder='Ingrese el correo'
 									/>
@@ -174,14 +279,11 @@ export default function CrearDeportista() {
 									<select
 										name="club"
 										value={datosNuevoDeportista.club}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('club', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 									>
 										<option value="">Seleccione un club</option>
-										<option value="norte">Norte</option>
-										<option value="sur">Sur</option>
-										<option value="este">Este</option>
-										<option value="oeste">Oeste</option>
+										<OpcionesClubes/>
 									</select>
 								</div>
 							</div>
@@ -195,22 +297,40 @@ export default function CrearDeportista() {
 									<select
 										name="categoria"
 										value={datosNuevoDeportista.categoria}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('categoria', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 px-4 text-black'
 									>
 										<option value="">Seleccione una categoria</option>
-										<option value="pluma">Pluma</option>
-										<option value="pesado">Pesado</option>
-										<option value="mosca">Mosca</option>
+										<OpcionesCategorias/>
 									</select>
 								</div>
 							</div>
+
+							<div className="flex">
+								<div className="w-1/3 mx-2">
+									<div className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 flex items-center justify-center text-black' id='texto-general'>
+                                        Fecha de nacimiento
+									</div>
+								</div>
+								<div className="w-2/3 mx-2" id='texto-general'>
+									<input
+										type="date"
+										name="fecha"
+										value={fechaCompleta(datosNuevoDeportista.fecha)}
+										onChange={(e) => handleChangeFecha('fecha', e.target.value)}
+										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
+										min="1900-01-01"
+										required
+									/>
+								</div>
+							</div>
+
 						</div>
 					</div>
 					<div className="mt-5 flex justify-center">
 						<button
 							type="button"
-							//onClick={handleGuardarCambios}
+							onClick={handleGuardarCambios}
 							className='bg-[#cd1919] mx-5 w-60 h-10 text-white py-2 px-4 rounded-lg'
 						>
                             Guardar cambios
---
+++ components/OpcionesCategorias.tsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface Option{
+    value: string,
+    label: string,
+}
+
+export default function OpcionesCategorias(){
+	const [opciones, setOpciones] = useState<Option[]>([]);
+	const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+	var cargado = false;
+	useEffect(() => {
+		const datos = localStorage.getItem('userData');
+		var arreglo;
+
+		if (datos != null) {
+			arreglo = JSON.parse(datos);
+		}
+		carga(arreglo);
+		cargado = true;
+	}, [!cargado]);
+
+	async function carga(datos: { token: any}): Promise<void> {
+		try {
+			const headers = {
+				sessiontoken: datos.token
+			};
+
+			const response = await axios.get(`${apiEndpoint}/weightCategory/List`, {
+				headers: headers
+			});
+
+			const mappedOptions = response.data.weightCategory.map((item: any) => ({
+				value: item._id,
+				label: item.name,
+			}));
+			setOpciones(mappedOptions);
+
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	return (
+		<>
+			{opciones.map((option) => (
+				<option key={option.value} value={option.value}>
+					{option.label}
+				</option>
+			))}
+		</>
+	);
+}
