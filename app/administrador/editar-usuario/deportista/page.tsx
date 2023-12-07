087b3d44e17f16ca1d608c5dc32926dad1dd1a35
Tkalejadro122 <tkalejadro122@gmail.com>
2023-12-06 20:28:46 -0500

    Completo (no todo)

--- app/administrador/editar-usuario/deportista/page.tsx
+++ app/administrador/editar-usuario/deportista/page.tsx
@@ -1,41 +1,128 @@
 'use client';
 
-import { ChangeEvent, useState } from 'react';
+import OpcionesCategorias from '@/components/OpcionesCategorias';
+import OpcionesClubes from '@/components/OpcionesClubes';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 interface FormData {
-  nombre: string;
-  apellido: string;
-  documento: string;
-  direccion: string;
-  telefono: string;
-  correo: string;
-  club: string;
-  categoria: string;
-}
+	name: string;
+	lastName: string;
+	phone: string;
+	club: string;
+	weightCategory: string;
+	weight: number;
+  }
 
 export default function EditarDeportista() {
 
+	//TRAER ID DE USUARIO DE LA URL
+	const valor = useSearchParams();
+	const id = valor.get('id');
+
 	const [datosDeportista, setDatosDeportista] = useState<FormData>({
-		nombre: '',
-		apellido: '',
-		documento: '',
-		direccion: '',
-		telefono: '',
-		correo: '',
+		name: '',
+		lastName: '',
+		phone: '',
 		club: '',
-		categoria: '',
+		weightCategory: '',
+		weight: 0,
 	});
 
-	const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-		setDatosDeportista((prevData) => ({
-			...prevData,
+	const handleChange = (field: keyof FormData, value: string) => {
+		setDatosDeportista((prevFormData) => ({
+			...prevFormData,
+			[field]: value
 		}));
 	};
+
+	//Método de cargar los usuarios del localStorage
+	const cargarUsuarios = async () => {
+		const datos = localStorage.getItem('userData');
+		var arreglo;
+
+		if (datos != null) {
+			arreglo = JSON.parse(datos);
+		}
+		const dataDeportista = await cargaDeportista(arreglo);
+		setDatosDeportista(dataDeportista.data.user);
+	};
+
+	//Consumir endpoint
+	const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+	async function cargaDeportista(datos: { token: any }): Promise<any> {
+		try {
+			const headers = {
+				sessiontoken: datos.token
+			};
+			const parametros = {
+				userId: id
+			};
+
+			const response = await axios.get(`${apiEndpoint}/users`, {
+				params: parametros,
+				headers: headers
+			});
+
+			return response;
+
+			//console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	//FUNCIÓN PARA GUARDAR LA INFO Y ENVIAR A LA BASE DE DATOS
+	async function handleGuardarCambios(): Promise<void> {
+		const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+		try{
+			const datos = localStorage.getItem('userData');
+			var arreglo;
+
+			if (datos != null) {
+				arreglo = JSON.parse(datos);
+			}
+
+			const parametro = {
+				userId: id,
+			};
+
+			const cabeza = {
+				sessiontoken: arreglo.token,
+			};
+
+			const body = {
+				name: datosDeportista.name,
+				lastName: datosDeportista.lastName,
+				weight: Number(datosDeportista.weight),
+				phone: datosDeportista.phone,
+				club: datosDeportista.club,
+				weightCategory: datosDeportista.weightCategory,
+			};
+
+			const response = await axios.patch(`${apiEndpoint}/users/Deportista`, body, {
+				headers: cabeza,
+				params: parametro,
+			});
+			window.location.href = `/administrador/info-usuario/deportista?id=${response.data.user._id}`;
+		} catch (error) {
+			console.log(error);
+		}
+
+	};
+
+	//UseEffect para pruebas
+	var cargado = false;
+	useEffect(() => {
+		cargarUsuarios();
+		cargado = true;
+	}, [!cargado]);
 
 	return (
 		<>
 			<div className="container mx-auto mt-8">
-				<h1 className='text-center text-[400%]' id='titulos-grandes'>CREAR DEPORTISTA</h1>
+				<h1 className='text-center text-[400%]' id='titulos-grandes'>EDITAR DEPORTISTA</h1>
 				<div className='flex items-center justify-center'>
 					<svg
 						className="my-1"
@@ -60,8 +147,8 @@ export default function EditarDeportista() {
 									<input
 										type="text"
 										name="nombre"
-										value={datosDeportista.nombre}
-										onChange={handleInputChange}
+										value={datosDeportista.name}
+										onChange={(e) => handleChange('name', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 									/>
 								</div>
@@ -76,8 +163,8 @@ export default function EditarDeportista() {
 									<input
 										type="text"
 										name="apellido"
-										value={datosDeportista.apellido}
-										onChange={handleInputChange}
+										value={datosDeportista.lastName}
+										onChange={(e) => handleChange('lastName', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 									/>
 								</div>
@@ -85,32 +172,17 @@ export default function EditarDeportista() {
 							<div className="flex">
 								<div className="w-1/3 mx-2">
 									<div className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 flex items-center justify-center text-black' id='texto-general'>
-                                        Documento:
+                                        Peso
 									</div>
 								</div>
 								<div className="w-2/3 mx-2" id='texto-general'>
 									<input
-										type="text"
-										name="documento"
-										value={datosDeportista.documento}
-										onChange={handleInputChange}
+										type="number"
+										name="peso"
+										value={datosDeportista.weight}
+										onChange={(e) => handleChange('weight', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
-									/>
-								</div>
-							</div>
-							<div className="flex">
-								<div className="w-1/3 mx-2">
-									<div className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 flex items-center justify-center text-black' id='texto-general'>
-                                        Dirección
-									</div>
-								</div>
-								<div className="w-2/3 mx-2" id='texto-general'>
-									<input
-										type="text"
-										name="direccion"
-										value={datosDeportista.direccion}
-										onChange={handleInputChange}
-										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
+										placeholder='Ingresa el peso'
 									/>
 								</div>
 							</div>
@@ -126,24 +198,8 @@ export default function EditarDeportista() {
 									<input
 										type="text"
 										name="telefono"
-										value={datosDeportista.telefono}
-										onChange={handleInputChange}
-										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
-									/>
-								</div>
-							</div>
-							<div className="flex">
-								<div className="w-1/3 mx-2">
-									<div className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 flex items-center justify-center text-black' id='texto-general'>
-                                        Correo
-									</div>
-								</div>
-								<div className="w-2/3 mx-2" id='texto-general'>
-									<input
-										type="text"
-										name="correo"
-										value={datosDeportista.correo}
-										onChange={handleInputChange}
+										value={datosDeportista.phone}
+										onChange={(e) => handleChange('phone', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 									/>
 								</div>
@@ -158,14 +214,10 @@ export default function EditarDeportista() {
 									<select
 										name="club"
 										value={datosDeportista.club}
-										onChange={handleInputChange}
+										onChange={(e) => handleChange('club', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 pl-4 text-black'
 									>
-										<option value="">Seleccione un club</option>
-										<option value="norte">Norte</option>
-										<option value="sur">Sur</option>
-										<option value="este">Este</option>
-										<option value="oeste">Oeste</option>
+										<OpcionesClubes/>
 									</select>
 								</div>
 							</div>
@@ -178,14 +230,11 @@ export default function EditarDeportista() {
 								<div className="w-2/3 mx-2" id='texto-general'>
 									<select
 										name="categoria"
-										value={datosDeportista.categoria}
-										onChange={handleInputChange}
+										value={datosDeportista.weightCategory}
+										onChange={(e) => handleChange('weightCategory', e.target.value)}
 										className='bg-neutral-200 rounded-full w-full h-10 mx-5 my-2 px-4 text-black'
 									>
-										<option value="">Seleccione una categoria</option>
-										<option value="pluma">Pluma</option>
-										<option value="pesado">Pesado</option>
-										<option value="mosca">Mosca</option>
+										<OpcionesCategorias/>
 									</select>
 								</div>
 							</div>
@@ -193,6 +242,7 @@ export default function EditarDeportista() {
 					</div>
 					<div className="mt-5 flex justify-center">
 						<button
+							onClick={handleGuardarCambios}
 							type="button"
 							className='bg-[#cd1919] mx-5 w-60 h-10 text-white py-2 px-4 rounded-lg'
 						>
--- app/administrador/lista-usuarios/entrenador/page.tsx
+++ app/administrador/lista-usuarios/entrenador/page.tsx
@@ -5,6 +5,7 @@ import Table from '@/components/tablas/Table';
 import ModalArchivo from '@/components/ModalArchivo/ModalArchivo';
 import data from '@/pruebas/usuarios.json';
 import Link from 'next/link';
+import axios from 'axios';
 
 const Home: React.FC = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -26,12 +27,58 @@ const Home: React.FC = () => {
 		}
 	};
 
+	const handleDownload = async () => {
+		const datos = localStorage.getItem('userData');
+		let json;
+
+		if (datos !== null) {
+			json = JSON.parse(datos);
+			console.log(json);
+		}
+
+		const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+		try {
+			const headers = {
+				sessiontoken: json.token
+			};
+
+			const response = await axios.get(`${apiEndpoint}/users/download/Entrenador`, {
+				headers: headers,
+				responseType: 'blob'
+			});// Accede a la propiedad 'data' de la respuesta
+
+			if (response.status === 200) {
+				// Convertir la respuesta a un blob (archivo)
+				const blob = response.data;
+
+				// Crear un objeto URL para el blob
+				const url = window.URL.createObjectURL(new Blob([blob]));
+
+				// Crear un enlace temporal y simular el clic para descargar el archivo
+				const link = document.createElement('a');
+				link.href = url;
+				link.setAttribute('download', 'Reporte_entrenadores.xlsx'); // Nombre del archivo
+				document.body.appendChild(link);
+				link.click();
+
+				// Liberar el objeto URL creado
+				setTimeout(() => {
+					window.URL.revokeObjectURL(url);
+					document.body.removeChild(link);
+				  }, 1000);
+			}
+
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return (
 		<div className="w-full max-w-screen-xl mx-auto mt-[6%] p-6">
-			<Table rol='Entrenador' link='/administrador/info-usuario/entrenador'/>
+			<Table rol='Entrenador' linkVer='/administrador/info-usuario/entrenador' linkEditar= '/administrador/editar-usuario/entrenador'/>
 			<div className="flex justify-between items-center mt-4">
 				<button
-					onClick={() => alert('Usuarios descargados')}
+					onClick={handleDownload}
 					className="bg-[#cd1919] text-white rounded p-2"
 				>
 					Descargar usuarios
