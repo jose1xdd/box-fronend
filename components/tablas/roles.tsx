385ec0903f5f65366720bfed2a40cb9b965974f3
Juliana <lizethjuliananava@ufps.edu.co>
2024-05-04 22:42:22 -0500

    Vistas despliegue primera iteracion

--- components/tablas/roles.tsx
+++ components/tablas/roles.tsx
@@ -9,7 +9,7 @@ export default function TableRoles() {
 			<thead>
 				<tr>
 					<th
-						className="border-[#1e1e1e] border-[8px] p-3 bg-[#cd1919] text-white text-center text-[150%] tracking-widest"
+						className="border-[#1e1e1e] border-[4px] p-3 bg-[#cd1919] text-white text-center text-[150%] tracking-widest"
 						id="titulos-grandes"
 					>
                         Rol
@@ -19,7 +19,7 @@ export default function TableRoles() {
 			<tbody>
 				{rolesUnicos.map((rol, index) => (
 					<tr key={index}>
-						<td className=" border-[#1e1e1e] border-[8px] p-3 bg-[#dfdfdf] text-center text-black">
+						<td className=" border-[#1e1e1e] border-[4px] p-3 bg-white text-center text-black">
 							{rol}
 						</td>
 					</tr>
