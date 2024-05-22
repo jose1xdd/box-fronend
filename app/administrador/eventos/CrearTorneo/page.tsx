c8d26195f60a2d990f429519478cfa21c6fd094b
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2024-05-21 21:31:22 -0500

    Checkpoint

---
+++ app/administrador/eventos/CrearTorneo/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+export default function CrearTorneo() {
+	return (
+		<div>Torneos</div>
+	);
+}
---
+++ app/administrador/ranking/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+import Ranking from '@/components/ranking/page';
+
+import RankingTable from '@/components/tablas/ranking';
+import axios from 'axios';
+import {
+	useCallback,
+	useEffect,
+	useState
+} from 'react';
+
+export default function RankingAdmin() {
+
+	const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+	const [info, setInfo] = useState<tablaRanking[]>([]);
+
+	const carga = useCallback(async (datos: { token: any }) => {
+		try {
+			const headers = {
+				sessiontoken: datos.token
+			};
+
+			const parametros = {
+				role: 'Deportista'
+			};
+
+			const response = await axios.get(`${apiEndpoint}/users/List`, {
+				params: parametros,
+				headers: headers
+			});
+
+			const data: tablaRanking[] = [];
+
+			for (const user of response.data.users) {
+				const ranking: ranking = {
+					win: user.ranking.win,
+					lose: user.ranking.lose,
+					draw: user.ranking.draw,
+				};
+				const dato: tablaRanking = {
+					nombre: user.name,
+					apellido: user.lastName,
+					id: user._id,
+					ranking
+				};
+				data.push(dato);
+			}
+
+			// Actualizar el estado con los datos recibidos
+			setInfo(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}, [apiEndpoint, setInfo]);
+
+	useEffect(() => {
+		const datos = localStorage.getItem('userData');
+		var json;
+
+		if (datos != null) {
+			json = JSON.parse(datos);
+			carga(json);
+		}
+	}, [carga]);
+
+	return (
+		<RankingTable data={info} />
+	);
+}
